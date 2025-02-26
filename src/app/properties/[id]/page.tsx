9a609d8e72ffcdd0df4ea48c6ad2a25d4de73d5a
Mr J <274924+jmxt3@users.noreply.github.com>
2025-02-26 17:16:57 -0300

    Refactor ID property page to accept parameters and display property ID; update dependencies in package.json

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -1,5 +1,14 @@
-const IdPropertyPage = () => {
-    return (<div className="flex flex.row justify-between pt-[100px] pl-5">ID Property Page</div>);
+interface Params {
+    id: string;
+}
+
+const IdPropertyPage = ({ params }: { params: Params }) => {
+
+    return (
+        <div className="pt-[100px] pl-5">
+            <p>ID Property Page {params.id}</p>
+        </div>
+    );
 }
 
 export default IdPropertyPage;
