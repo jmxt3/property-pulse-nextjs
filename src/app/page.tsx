8e7c66158bbd4178ea18e98dce30767f6243a28f
Mr J <274924+jmxt3@users.noreply.github.com>
2025-03-02 23:31:08 -0300

    Add HomeProperties component to display recent properties on the HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from '../components/hero';
 import InfoBoxes from '@/components/infoBoxes';
 import Footer from '@/components/footer';
+import HomeProperties from '@/components/homeProperties';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
       <Footer />
     </>
   );
